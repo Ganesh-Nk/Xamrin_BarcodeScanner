83dd8d8cc8c195908b1cd851d23c6d97627b5842
Ganesh Navada K <gnakx@eg.dk>
2025-03-07 20:34:51 +0530

    small updates

--- YourShoppingAPP.Android/MainActivity.cs
+++ YourShoppingAPP.Android/MainActivity.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
-
 namespace YourShoppingAPP.Droid
 {
     [Activity(Label = "YourShoppingAPP", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
